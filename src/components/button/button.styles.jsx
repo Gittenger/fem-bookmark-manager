731ba106ac357e999e4819e294bf3efffd1560d0
gittenger <john.pittenger86@gmail.com>
2020-05-15 16:47:14 -0400

    button hover styles

--- src/components/button/button.styles.jsx
+++ src/components/button/button.styles.jsx
@@ -1,6 +1,6 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-export const StyledButton = styled.button`
+const ButtonStyles = css`
   padding: 1rem 2em;
   background-color: ${({ theme: { colors }, color }) =>
     color === "blue"
@@ -18,36 +18,51 @@ export const StyledButton = styled.button`
   border-radius: 0.7rem;
   font-size: 1.4rem;
   font-weight: 500;
+  transition: all 0.3s;
 
   &:focus {
     outline: none;
     box-shadow: ${({ theme: { colors } }) =>
       `0 0 0 1px ${colors.softBlue}, 0 1px 1px 1px rgba(0, 0, 0, 0.7)`};
+  }
+
+  &:hover {
+    background-color: ${({ theme: { colors } }) => colors.white};
+    color: ${({ theme: { colors }, color }) =>
+      color === "blue"
+        ? `${colors.softBlue}`
+        : color === "red"
+        ? `${colors.softRed}`
+        : `${colors.darkBlue}`};
+    border: ${({ theme: { colors }, color }) =>
+      color === "blue"
+        ? `1.5px solid ${colors.softBlue}`
+        : color === "red"
+        ? `1.5px solid ${colors.softRed}`
+        : `1.5px solid ${colors.darkBlue}`};
+  }
+
+  &:active {
+    background-color: ${({ theme: { colors }, color }) =>
+      color === "blue"
+        ? `${colors.softBlue}`
+        : color === "red"
+        ? `${colors.softRed}`
+        : `${colors.darkBlue}`};
+    color: ${({ theme: { colors } }) => colors.white};
+    border: ${({ theme: { colors }, color }) =>
+      color === "blue"
+        ? `1.5px solid ${colors.softBlue}`
+        : color === "red"
+        ? `1.5px solid ${colors.softRed}`
+        : `1.5px solid ${colors.darkBlue}`};
   }
 `;
 
-export const StyledSubmitButton = styled.input`
-  padding: 1rem 2em;
-  background-color: ${({ theme: { colors }, color }) =>
-    color === "blue"
-      ? `${colors.softBlue}`
-      : color === "red"
-      ? `${colors.softRed}`
-      : `${colors.greyLight}`};
-  color: ${({ theme: { colors }, color }) =>
-    color === "blue"
-      ? `${colors.white}`
-      : color === "red"
-      ? `${colors.white}`
-      : `${colors.darkBlue}`};
-  cursor: pointer;
-  border-radius: 0.7rem;
-  font-size: 1.4rem;
-  font-weight: 500;
+export const StyledButton = styled.button`
+  ${ButtonStyles}
+`;
 
-  &:focus {
-    outline: none;
-    box-shadow: ${({ theme: { colors } }) =>
-      `0 0 0 1px ${colors.softBlue}, 0 1px 1px 1px rgba(0, 0, 0, 0.7)`};
-  }
+export const StyledSubmitButton = styled.input`
+  ${ButtonStyles}
 `;
