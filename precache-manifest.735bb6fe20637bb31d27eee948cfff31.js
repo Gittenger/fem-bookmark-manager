c4fff8a5bfb32cfc646976cb3e2499aeda891b00
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-05-06 13:28:07 -0400

    Updates

---
+++ precache-manifest.735bb6fe20637bb31d27eee948cfff31.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f1625de17aa7382c29fa233cff005da",
+    "url": "/fem-bookmark-manager/index.html"
+  },
+  {
+    "revision": "2ac0d667bc34199eab90",
+    "url": "/fem-bookmark-manager/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "745fc02b35b3b691c7d4",
+    "url": "/fem-bookmark-manager/static/js/2.02719b38.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/fem-bookmark-manager/static/js/2.02719b38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ac0d667bc34199eab90",
+    "url": "/fem-bookmark-manager/static/js/main.ab0abc4d.chunk.js"
+  },
+  {
+    "revision": "3ea6008f0aabf9a10019",
+    "url": "/fem-bookmark-manager/static/js/runtime-main.1003ba05.js"
+  },
+  {
+    "revision": "7c66c472eb50c9abe027977b6f2b406e",
+    "url": "/fem-bookmark-manager/static/media/bg-dots.7c66c472.svg"
+  },
+  {
+    "revision": "38993f94ba58120b39289a5258af7063",
+    "url": "/fem-bookmark-manager/static/media/icon-arrow.38993f94.svg"
+  },
+  {
+    "revision": "c24c1904c0c62485f4200c3a49a45a27",
+    "url": "/fem-bookmark-manager/static/media/icon-facebook.c24c1904.svg"
+  },
+  {
+    "revision": "e3defca190ae3f2aaeedefe79c125bd6",
+    "url": "/fem-bookmark-manager/static/media/icon-hamburger.e3defca1.svg"
+  },
+  {
+    "revision": "a4daae10e6bd237b96908d16566647e2",
+    "url": "/fem-bookmark-manager/static/media/icon-twitter.a4daae10.svg"
+  },
+  {
+    "revision": "25a237330ac7fab409f2b5833363ae97",
+    "url": "/fem-bookmark-manager/static/media/illustration-features-tab-1.25a23733.svg"
+  },
+  {
+    "revision": "11b0ec333ed9d17690ada04979c489c0",
+    "url": "/fem-bookmark-manager/static/media/illustration-features-tab-2.11b0ec33.svg"
+  },
+  {
+    "revision": "7f696303479b76b89e45fcf33b7aff4b",
+    "url": "/fem-bookmark-manager/static/media/illustration-features-tab-3.7f696303.svg"
+  },
+  {
+    "revision": "dfa0cd821469019e297c8c27cfc0d475",
+    "url": "/fem-bookmark-manager/static/media/illustration-hero.dfa0cd82.svg"
+  },
+  {
+    "revision": "52986ee874eb84ff07d7e5da7d8953b2",
+    "url": "/fem-bookmark-manager/static/media/logo-bookmark-white.52986ee8.svg"
+  },
+  {
+    "revision": "53989d2a42baf707e1d92f5d05c7899b",
+    "url": "/fem-bookmark-manager/static/media/logo-bookmark.53989d2a.svg"
+  },
+  {
+    "revision": "3e1ab00bc1a7182ca9297e73eaa98480",
+    "url": "/fem-bookmark-manager/static/media/logo-chrome.3e1ab00b.svg"
+  },
+  {
+    "revision": "e5023c2b57490fa1d28da6dea398606b",
+    "url": "/fem-bookmark-manager/static/media/logo-firefox.e5023c2b.svg"
+  },
+  {
+    "revision": "3d8f7bcc99ea84508c6a3380e0ae0878",
+    "url": "/fem-bookmark-manager/static/media/logo-opera.3d8f7bcc.svg"
+  }
+]);
